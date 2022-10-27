61f5d7db642946a310120da39a94f2407da92379
shreyangvayla <shreyangvayla23032001@gmail.com>
2022-10-27 17:49:29 +0530

    Querysets

--- leads/models.py
+++ leads/models.py
@@ -1,4 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+# User = get_user_model()
+
+class User(AbstractUser):
+    pass
 
 class Lead(models.Model):   
     first_name = models.CharField(max_length=10)
@@ -6,6 +12,11 @@ class Lead(models.Model):
     age = models.IntegerField(default=0)
     agent = models.ForeignKey("Agent",on_delete=models.CASCADE)
 
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
 class Agent(models.Model):
-    first_name = models.CharField(max_length=10)
-    last_name = models.CharField(max_length=10)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.email
