63fedee0a7d21d0c0c9d2ab5b1b1b9a31d157051
shreyangvayla <shreyangvayla23032001@gmail.com>
2022-12-02 08:43:12 +0530

    Assign agent

--- leads/forms.py
+++ leads/forms.py
@@ -1,14 +1,9 @@
 from django import forms
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm, UsernameField
-from .models import Lead
+from .models import Lead,Agent,Category
 
 User = get_user_model()
-
-class LeadForm(forms.Form):
-    first_name = forms.CharField(max_length=10)
-    last_name = forms.CharField(max_length=10)
-    age = forms.IntegerField(min_value=0)
 
 class LeadModelForm(forms.ModelForm):
     class Meta:
@@ -18,10 +13,40 @@ class LeadModelForm(forms.ModelForm):
             'last_name',
             'age',
             'agent',
+            
         )
+
+
+class LeadForm(forms.Form):
+    first_name = forms.CharField(max_length=10)
+    last_name = forms.CharField(max_length=10)
+    age = forms.IntegerField(min_value=0)
+
 
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ("username",)
         field_classes = {'username': UsernameField}
+
+class AssignAgentForm(forms.Form):
+    agent = forms.ModelChoiceField(queryset=Agent.objects.none())
+
+    def __init__(self, *args, **kwargs):
+        request = kwargs.pop("request")
+        agents = Agent.objects.filter(organisation=request.user.userprofile)
+        super(AssignAgentForm, self).__init__(*args, **kwargs) 
+        self.fields["agent"].queryset = agents
+
+# class LeadCategoryUpdateForm(forms.ModelForm):
+#     class Meta:
+#         model = Lead
+#         fields = ('category',)
+
+
+# class CategoryModelForm(forms.ModelForm):
+#     class Meta:
+#         model = Category
+#         fields = (
+#             'name',
+#         )
--- leads/models.py
+++ leads/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.db.models.signals import post_save, pre_save
+from django.db.models.signals import post_save
 from django.contrib.auth.models import AbstractUser
 
 # User = get_user_model()
@@ -21,6 +21,11 @@ class Lead(models.Model):
     age = models.IntegerField(default=0)
     organisation = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
     agent = models.ForeignKey("Agent",null=True,blank=True,on_delete=models.SET_NULL)
+    Category = models.ForeignKey("Category", related_name="leads",null=True,blank=True,on_delete=models.SET_NULL)
+    # description = models.TextField()
+    # date_added = models.DateTimeField(auto_now_add=True)
+    # Phone_number = models.CharField(max_length=20)
+    # email = models.EmailField()
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
@@ -32,10 +37,15 @@ class Agent(models.Model):
     def __str__(self):
         return self.user.email
 
+class Category(models.Model):
+    name = models.CharField(max_length=30)   #New, Contacted, Converted, Unconverted
+    organisation = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
 def post_user_created_signal(sender, instance, created, **kwargs):
     if created:
         UserProfile.objects.create(user=instance)
 
-
-        
 post_save.connect(post_user_created_signal, sender=User)
